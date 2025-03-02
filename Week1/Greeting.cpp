814802df084d17d67b38bb452b7bb2390c45f641
Joski254 <kjosephtoo@gmail.com>
2025-03-02 10:51:56 -0600

    New file

---
+++ Week1/Greeting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string name;
+
+    std::cout << "Joseph: ";
+    std::cin >> name;
+
+    std::cout << "It is nice to meet you, " << name << "!" << std::endl;
+
+    return 0;
+}
