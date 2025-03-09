3206da2969f21310a0cc2409921165eb6f1863db
Joski254 <kjosephtoo@gmail.com>
2025-03-09 15:24:49 -0500

    Update Countdown.cpp

--- Countdown.cpp
+++ Countdown.cpp
@@ -4,16 +4,13 @@ using namespace std;
 int main() {
     int startNumber;
 
-    // Prompt the user for input
     cout << "Enter a number to start the countdown: ";
     cin >> startNumber;
 
-    // For loop to count down from the user-specified number to 1
     for (int i = startNumber; i >= 1; i--) {
         cout << i << endl;
     }
 
-    // Print the final message
     cout << "Blast off!" << endl;
 
     return 0;
